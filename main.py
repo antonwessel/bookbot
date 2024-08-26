8cbba82f58649a6e24e955c32c4a235a4d285461
antonwessel <wessel.anton@outlook.com>
2024-08-26 09:29:09 +0200

    count words

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_contents(book_path)
+    book_words_count = count_words(book_text)
+
+
+def get_book_contents(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words(text):
+    book_list_length = len(text.split())
+    return book_list_length
 
 
 if __name__ == "__main__":
